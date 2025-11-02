871b263993923a2090f3b1016141b3f9d661c5aa
Janith Madarasinghe <janithmadarasinghe@gmail.com>
2025-11-02 22:59:42 +0530

    new assets

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,23 @@ export const metadata: Metadata = {
   description:
     "pwaapp is a Progressive Web App example.",
   manifest: "/web.manifest",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: "Effi Rental"
+  },
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" }
+    ],
+    apple: [
+      { url: "/apple-touch-icon.png", sizes: "180x180" },
+      { url: "/apple-icon-180.png", sizes: "180x180" }
+    ],
+    other: [{ rel: "mask-icon", url: "/safari-pinned-tab.svg", color: "#0ea5e9" }]
+  }
 };
 
 export default function RootLayout({
