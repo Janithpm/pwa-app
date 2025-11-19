e902f02ab32d9984d697c8bdad07f8b671c5b4fc
Janith Madarasinghe <janithmadarasinghe@gmail.com>
2025-11-19 17:04:19 +0530

    new auth

---
+++ src/app/auth/layout.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { ReactNode, useEffect } from "react";
+import { Command } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
+import { APP_CONFIG } from "@/config/app-config";
+import { authClient } from "@/lib/auth/auth-client";
+import { useRouter } from "next/navigation";
+
+export default function AuthLayout({ children }: Readonly<{ children: ReactNode }>) {
+    //Todo: make this more efficient and faster some how
+    const router = useRouter();
+    useEffect(() => {
+        authClient.getSession().then((session) => {
+            if (session.data) {
+                router.push("/");
+            }
+        });
+    }, [router]);
+
+    return (
+        <main>
+            <div className="grid h-dvh justify-center p-2 lg:grid-cols-2">
+                <div className="bg-primary relative order-2 hidden h-full rounded-3xl lg:flex">
+                    <div className="text-primary-foreground absolute top-10 space-y-1 px-10">
+                        <Command className="size-10" />
+                        <h1 className="text-2xl font-medium">{APP_CONFIG.name}</h1>
+                        <p className="text-sm">Real-time fleet insights, seamless reservations.</p>
+                    </div>
+
+                    <div className="absolute bottom-10 flex w-full justify-between px-10">
+                        <div className="text-primary-foreground flex-1 space-y-1">
+                            <h2 className="font-medium">Manage your fleet with confidence</h2>
+                            <p className="text-sm">Track vehicle availability, maintenance schedules. Keep every car on the road and every booking running smoothly.</p>
+                        </div>
+                        <Separator orientation="vertical" className="mx-3 h-auto!" />
+                        <div className="text-primary-foreground flex-1 space-y-1">
+                            <h2 className="font-medium">Need assistance?</h2>
+                            <p className="text-sm">
+                                Browse our documentation or reach out to support anytime. We&apos;re here to help your rental service scale.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="relative order-1 flex h-full">{children}</div>
+            </div>
+        </main>
+    );
+}
---
+++ src/app/auth/sign-in/sign-in-form.tsx
@@ -0,0 +1,103 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth/auth-client";
+import { type SignInFormData, SignInFormSchema } from "@/zod/auth-forms";
+
+export function SignInForm() {
+    const form = useForm<SignInFormData>({
+        resolver: zodResolver(SignInFormSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+            remember: false,
+        },
+    });
+
+    const handleSignIn = async (data: SignInFormData) => {
+        try {
+            await authClient.signIn.email(
+                { ...data, callbackURL: "/" },
+                {
+                    onError: (error) => {
+                        toast.error('Sign in failed: ');
+                        console.log(error);
+                    },
+                    onSuccess: () => {
+                        toast.success("Successfully signed in!");
+                    }
+                }
+            )
+        } catch (error) {
+            toast.error("An unexpected error occurred. Please try again.");
+            console.log(error);
+        }
+    };
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(handleSignIn)} className="space-y-4">
+                <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Email Address</FormLabel>
+                            <FormControl>
+                                <Input id="email" type="email" placeholder="you@example.com" autoComplete="email" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Password</FormLabel>
+                            <FormControl>
+                                <Input
+                                    id="password"
+                                    type="password"
+                                    placeholder="••••••••"
+                                    autoComplete="current-password"
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="remember"
+                    render={({ field }) => (
+                        <FormItem className="flex flex-row items-center">
+                            <FormControl>
+                                <Checkbox
+                                    id="login-remember"
+                                    checked={field.value}
+                                    onCheckedChange={field.onChange}
+                                    className="size-4"
+                                />
+                            </FormControl>
+                            <FormLabel htmlFor="login-remember" className="text-muted-foreground ml-1 text-sm font-medium">
+                                Remember me for 30 days
+                            </FormLabel>
+                        </FormItem>
+                    )}
+                />
+                <Button className="w-full" type="submit">
+                    Login
+                </Button>
+            </form>
+        </Form>
+    );
+}
